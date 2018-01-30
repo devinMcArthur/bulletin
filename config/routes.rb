095c7bf7e97165310256865c901a4aef2e85a4c2
devinMcArthur <assortedgerm@gmail.com>
2018-01-30 02:17:04 +0000

    Add password reset functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
  
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -19,5 +23,6 @@ Rails.application.routes.draw do
   # Create RESTful functionality, and many named routes ('rails console' -> 'rails routes' to check) 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   
 end
